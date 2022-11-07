2cbd1cb167b0f45e8d04f2ab2b3caa13edc7efd9
Bixash <ibkstha@gmail.com>
2022-11-07 09:21:18 +0545

    singly

--- dotDsa/singly/singly.c
+++ dotDsa/singly/singly.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <malloc.h>
+#include <process.h>
 
 struct node {
     int info;
@@ -9,106 +10,122 @@ struct node {
 };
 
 typedef struct node node;
+node *head, *last;
+
 
 void insertBegin (node **head, int elt) {
     node *temp;
     temp = (node*)malloc(sizeof(node));
-    temp->info =elt;
+    temp->info = elt;
     temp ->next = *head;
     *head = temp;
+    printf("elt %d inserted", elt);
 
 }
-void insertEnd (node **head, int elt) {
-    node *temp, *temp1;
-    temp = (node*)malloc(sizeof(node));
+// void insertEnd (node **head, int elt) {
+//     node *temp, *temp1;
+//     temp = (node*)malloc(sizeof(node));
 
-    temp->next =NULL;
-    temp ->info = elt;
+//     temp->next =NULL;
+//     temp ->info = elt;
 
-    if (*head == NULL)
-        (*head) = temp;
-    else {
-        temp1 = *head;
-        while (temp1-> next != NULL)
-        {
-            temp1 =temp1-> next;
-        }
+//     if (*head == NULL)
+//         (*head) = temp;
+//     else {
+//         temp1 = *head;
+//         while (temp1-> next != NULL)
+//         {
+//             temp1 =temp1-> next;
+//         }
         
-        temp1 ->next = temp;
-    }
+//         temp1 ->next = temp;
+//     }
 
-}
+// }
 
 
-void insertSpecified (node **head, int elt, int pos) {
+// void insertSpecified (node **head, int elt, int pos) {
 
-    node *temp *temp1;
+//     node *temp *temp1;
 
-    temp = (node*)malloc(sizeof(node));
+//     temp = (node*)malloc(sizeof(node));
 
-    temp->info =elt;
-    temp1 = *head;
+//     temp->info =elt;
+//     temp1 = *head;
 
-    if(head == NULL) {
-        printf("Void insertion");
-        exit(1);
-    }
+//     if(head == NULL) {
+//         printf("Void insertion");
+//         exit(1);
+//     }
 
-    for (int i = 1; i < pos-1; i++)
+//     for (int i = 1; i < pos-1; i++)
+//     {
+//         temp1= temp1->next ;
+        
+//         temp ->next = temp1 ->next;
+//         temp1 ->next = temp;
+//     }
+    
+//     temp ->next = *head;
+//     *head = temp;
+
+// }
+// int deleteBegin (node **head, int elt) {
+//     node *temp;
+//     int elt = -1;
+
+//     if (*head == NULL) 
+//      printf("list is empty");
+
+//     else {
+//         temp = *head;
+//         *head = temp -> next;
+//         elt = temp -> info;
+//         free(temp);
+
+//     }
+//     return elt;
+
+
+// }
+
+// int deleteEnd (node **head, int elt) {
+//    node *temp, *temp1;
+//    int elt = -1;
+
+//     if (head == NULL)
+//         prinf("\nList is empty");
+
+    
+
+// }
+// int deleteSpecified (node **head, int elt) {
+//     node *temp;
+//     temp = (node*)malloc(sizeof(node));
+//     temp->info =elt;
+//     temp ->next = *head;
+//     *head = temp;
+
+// }
+void displayElements () {
+    node *temp;
+    temp = head;
+     if (head == NULL)
+     {
+        printf("Empty link list");
+        exit(0);
+     }
+     
+    else
     {
-        temp1= temp1->next ;
-        
-        temp ->next = temp1 ->next;
-        temp1 ->next = temp;
+        while (temp != NULL)
+        {
+            printf("%d\t", temp->info);
+            temp = temp->next;
+        }
     }
-    
-    temp ->next = *head;
-    *head = temp;
-
-}
-int deleteBegin (node **head, int elt) {
-    node *temp;
-    int elt = -1;
-
-    if (*head == NULL) 
-     printf("list is empty");
-
-    else {
-        temp = *head;
-        *head = temp -> next;
-        elt = temp -> info;
-        free(temp);
-
-    }
-    return elt;
-
-
-}
-
-int deleteEnd (node **head, int elt) {
-   node *temp, *temp1;
-   int elt = -1;
-
-    if (head == NULL)
-        prinf("\nList is empty");
-
-    
-
-}
-int deleteSpecified (node **head, int elt) {
-    node *temp;
-    temp = (node*)malloc(sizeof(node));
-    temp->info =elt;
-    temp ->next = *head;
-    *head = temp;
-
-}
-int displayElements (node **head, int elt) {
-    node *temp;
-    temp = (node*)malloc(sizeof(node));
-    temp->info =elt;
-    temp ->next = *head;
-    *head = temp;
+     
+     
 
 }
 
@@ -119,32 +136,34 @@ void main () {
 
     node *head;
     head = NULL;
-    node *tail;
-    tail = NULL;
+    
 
     int elt;
     int choice;
 
     while (1)
     {
-        printf("\nOptions are :\n1. Push\n2. Pop\n3. Display\n4. Exit");
+        printf("\nOptions are :\n1. insert\n2. Pop\n3. Display\n4. Exit");
         printf("\nEnter your choice: ");
         scanf("%d",&choice);
 
 
         switch (choice)
         {
-            case 1:  push(&s);
+            case 1: printf("\nEnter element you want to add: ");
+                    scanf("%d", &elt);
+                    insertBegin (&head, elt);
 
             break;
 
             case 2: 
-                elt = pop(&s);
+               
                 printf("\n\'%d\'is popped\n", elt);
             break;
 
             case 3: 
-                display(&s);
+            printf("display");
+                void displayElements();
             
             break;
             
@@ -158,6 +177,6 @@ void main () {
         }
     }
 
-    insertBegin (&head, 5);
+    
 
 }
