27b99165d00f7fde91a23271c32ba8bfb4cc6449
Bixash <ibkstha@gmail.com>
2023-01-01 20:39:11 +0545

    student and distance create

--- dotJava/oop/distance/MyDistance.java
+++ dotJava/oop/distance/MyDistance.java
@@ -5,6 +5,7 @@
 
 package oop.distance;
 
+
 class Distance {
     private int feet;
     private float inches;
@@ -18,7 +19,14 @@ class Distance {
         this.inches = inches;
     }
 
-    public Distance addDistance(Distance d) {
+    public int getFeet() {
+        return feet;
+    }
+    public float getInches() {
+        return inches;
+    }
+
+    public void addDistance(Distance d) {
         Distance dd = new Distance();
         dd.feet = feet + d.feet;
         dd.inches = inches + d.inches;
@@ -27,16 +35,44 @@ class Distance {
             dd.feet += 1;
             dd.inches -= 12;  
         }
-        return dd;
+        System.out.println(dd.feet+"' "+dd.inches+"\" ");
     }
+
+
     public void compareDistance(Distance d) {
         if(feet > d.feet)
+            System.out.println("Greater: "+ feet+"' "+  inches+"\" ");
         else if(feet < d.feet)
+            System.out.println("Greater: "+ d.feet+"' "+  d.inches+"\" ");
+        else {
+            if(inches > d.inches)
+                System.out.println("Greater: "+ feet+"' "+  inches+"\" ");
+            else if(inches > d.inches)
+                System.out.println("Greater: "+ d.feet+"' "+  d.inches+"\" ");
+            else
+                System.out.println("Both are equal! ");
+        }
         
-        return (feet + inches);
     }
 
 }
+
+
+
+
 public class MyDistance {
+    public static void main(String[] args) {
+        
+        Distance d1 = new Distance(2,4);
+        Distance d2 = new Distance(3,5);
+        // Distance d3 = new Distance();
+
+       
+        System.out.print("Added: ");
+        d2.addDistance(d1);
     
+        d1.compareDistance(d2);
+
+        
+    } 
 }
--- dotJava/oop/student/MyStudent.java
+++ dotJava/oop/student/MyStudent.java
@@ -3,24 +3,35 @@
 // Use this class to find percentage and division of five students.
 
 package oop.student;
-
+import java.util.Scanner;
 class Student {
     public String name;
     public int roll;
-    public int[]marks = new int[5]; 
+    public int[]marks; 
+
+    static int sub = 5;
+    Scanner sc = new Scanner(System.in);
 
     public Student () {
 
     }
 
-    public Student (String name, int roll, int[] marks) {
+    public Student (String name, int roll, int[]marks) {
 
         this.name = name;
         this.roll = roll;
         this.marks = marks;
+        // getMarks();
         
     }
-    public int addTotal() {
+
+    // public void getMarks () {
+    //     for(int i = 0; i<sub; i++) {
+    //         marks[i]= sc.nextInt();
+    //     }
+
+    // }
+    public int total() {
         int sum = 0;
         for(int i=0; i < marks.length; i++) {
             
@@ -28,16 +39,40 @@ class Student {
         }
         return sum;
     }
-    public int findPercentage() {
+
+    public  int findPercent() {
         
-        return (int)(addTotal()/5);
+        return total()/sub;
     }
 }
 
 public class MyStudent {
     public static void main(String[] args) {
         
-        // Student s1 = new Student('b', 10, [10] );
+        int m[]={61,12,64,55,6};
+        Student s = new Student("Bikash", 1, m);
+        
+        int m1[]={11,52,44,55,6};
+        Student s1 = new Student("Aarati", 2, m1);
+
+        int m2[]={15,52,74,5,56};
+        Student s2 = new Student("Pooja", 3, m2);
+
+        int m3[]={13,20,54,5,6};
+        Student s3 = new Student("Aaradhana", 4, m3);
+
+        int m4[]={21,24,34,5,6};
+        Student s4 = new Student("Prasamsha", 5, m4);
+
+        System.out.println("\nName: "+s.name+ "| Roll no: "+s.roll+"| Total: "+s.total()+"| Percent: "+s.findPercent());
+        System.out.println("\nName: "+s1.name+ "| Roll: "+s1.roll+"| Total: "+s1.total()+"| Percent: "+s1.findPercent());
+        System.out.println("\nName: "+s2.name+ "| Roll: "+s2.roll+"| Total: "+s2.total()+"| Percent: "+s2.findPercent());
+        System.out.println("\nName: "+s3.name+ "| Roll: "+s3.roll+"| Total: "+s3.total()+"| Percent: "+s3.findPercent());
+        System.out.println("\nName: "+s4.name+ "| Roll: "+s4.roll+"| Total: "+s4.total()+"| Percent: "+s4.findPercent());
+          
+            
+        
+        
     }
 }
 
