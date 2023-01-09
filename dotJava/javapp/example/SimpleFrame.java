fde9c8895b7fb2051d47b2b76a2ca1bdfdf14f7b
Bixash <ibkstha@gmail.com>
2023-01-09 23:06:57 +0545

    jdbc connected

--- dotJava/javapp/example/SimpleFrame.java
+++ dotJava/javapp/example/SimpleFrame.java
@@ -1,64 +1,58 @@
 package example;
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SimpleFrame
-{
-    public static void main(String[] args) 
-    {
+public class SimpleFrame {
+    public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             JFrame f = new JFrame();
             f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            f.setSize(500,200);
+            f.setSize(500, 200);
             SimplePanel p = new SimplePanel();
             f.add(p);
             f.setVisible(true);
         });
-    }    
+    }
 }
-class SimplePanel extends JPanel
-{
+
+class SimplePanel extends JPanel {
     private final JButton b1, b2;
     private final JTextField t1, t2, t3;
     private final JLabel l1, l2, l3;
-    public SimplePanel()
-    {
-    b1 = new JButton("Add");
-	b2 = new JButton("Subtract");
-    t1 = new JTextField(8);  
-	t2 = new JTextField(8);
-	t3 = new JTextField(8);
-	l1 = new JLabel("Val1:");
-	l2 = new JLabel("Val2:");
-	l3 = new JLabel("Result:");
+
+    public SimplePanel() {
+        b1 = new JButton("Add");
+        b2 = new JButton("Subtract");
+        t1 = new JTextField(8);
+        t2 = new JTextField(8);
+        t3 = new JTextField(8);
+        l1 = new JLabel("Val1:");
+        l2 = new JLabel("Val2:");
+        l3 = new JLabel("Result:");
         MyEventListener me = new MyEventListener();
-        b1.addActionListener(me); 
-	b2.addActionListener(me);       
+        b1.addActionListener(me);
+        b2.addActionListener(me);
         add(l1);
-	add(t1);
-	add(l2);
-	add(t2);
-	add(b1);
-	add(b2);
-	add(l3);
-        add(t3);        
-    }  
-    private class MyEventListener implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e) 
-        {        
+        add(t1);
+        add(l2);
+        add(t2);
+        add(b1);
+        add(b2);
+        add(l3);
+        add(t3);
+    }
+
+    private class MyEventListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
             int x, y, z;
             x = Integer.parseInt(t1.getText());
             y = Integer.parseInt(t2.getText());
-
-            if(e.getActionCommand() == "Add")
-	            z = x + y;
-	        else
-		        z = x - y;
-                
+            if (e.getActionCommand() == "Add")
+                z = x + y;
+            else
+                z = x - y;
             t3.setText(String.valueOf(z));
         }
     }
--- dotJava/jdbc/database.java
+++ dotJava/jdbc/database.java
@@ -6,14 +6,14 @@ import java.sql.*;
 public class database {
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            // Class.forName("com.mysql.cj.jdbc.Driver");       // kind of deprecated
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pascal", "root", "");
             Statement stmt = conn.createStatement();
             String sqlStmt = "SELECT * FROM users";
             ResultSet rs = stmt.executeQuery(sqlStmt);
 
             while (rs.next())
-                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getInt(3));
+                System.out.println(rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4));
                 
             rs.close();
             stmt.close();
