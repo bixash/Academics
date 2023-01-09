6b004bc0f48c4576f2c0bee5fc2c9f737277d288
Bixash <ibkstha@gmail.com>
2023-01-09 22:13:09 +0545

    database added

--- dotJava/jdbc/database.java
+++ dotJava/jdbc/database.java
@@ -1,2 +1,25 @@
 // 1. Assuming your own database and tables write a program using JDBC to enter and
 // display data from the database. Use GUI form to enter and display data.
+
+import java.sql.*;
+
+public class database {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pascal", "root", "");
+            Statement stmt = conn.createStatement();
+            String sqlStmt = "SELECT * FROM users";
+            ResultSet rs = stmt.executeQuery(sqlStmt);
+
+            while (rs.next())
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getInt(3));
+                
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
