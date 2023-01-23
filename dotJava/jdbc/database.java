54b63a986df5e84a2c502cfa7324ad0b425b1baa
Bixash <ibkstha@gmail.com>
2023-01-23 21:02:05 +0545

    updated jdbc and test

--- dotJava/jdbc/database.java
+++ dotJava/jdbc/database.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class database {
     public static void main(String[] args) {
         try {
-            // Class.forName("com.mysql.cj.jdbc.Driver");       // kind of deprecated
+            // Class.forName("com.mysql.jdbc.Driver");       // kind of deprecated
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pascal", "root", "");
             Statement stmt = conn.createStatement();
             String sqlStmt = "SELECT * FROM users";
---
+++ dotJava/test.java
@@ -0,0 +1,14 @@
+class test
+{
+    static int a = 3;
+    static int b;
+    static {
+        System.out.println("Sayaashsd");
+        b= a * 4;
+    }
+
+   public static void main(String[] args) {
+        System.out.println("a="+a);
+        System.out.println("b ="+b);
+   }
+}
